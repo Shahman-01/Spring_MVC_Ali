ce2446966187081b7b476473710246c0c1540730
Shahman Magomadov <magomadovshahman@gmail.com>
2022-12-18 15:16:39 +0300

    lesson 21

---
+++ src/main/java/org/example/spring_mvc/dao/PersonDAO.java
@@ -0,0 +1,33 @@
+package org.example.spring_mvc.dao;
+
+import org.example.spring_mvc.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+	private static int PEOPLE_COUNT;
+	private List<Person> people;
+
+	{
+		people = new ArrayList<>();
+
+		people.add(new Person(++PEOPLE_COUNT, "Tom"));
+		people.add(new Person(++PEOPLE_COUNT, "Max"));
+		people.add(new Person(++PEOPLE_COUNT, "Dima"));
+		people.add(new Person(++PEOPLE_COUNT, "Gye"));
+	}
+
+	public List<Person> index() {
+		return people;
+	}
+
+	public Person show(int id) {
+		return people.stream()
+				.filter(p -> p.getId() == id)
+				.findAny()
+				.orElse(null);
+	}
+}
